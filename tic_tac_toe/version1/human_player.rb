35b95f87361bca292b43181b23323f51e3b9a56a
meihuang <meihuang8328@gmail.com>
2022-11-16 17:41:38 -0500

    Human Player

--- tic_tac_toe/version1/human_player.rb
+++ tic_tac_toe/version1/human_player.rb
@@ -1,11 +1,35 @@
 class HumanPlayer
 
     def initialize(mark_value)
-        @mark_value = mark_value
+        @mark= mark_value
     end
 
-    attr_reader :mark_value
+    attr_reader :mark
 
+    def get_position
+
+        print "enter a position with coordinated separated with a space like `4 7`"
+       
+        input = gets.chomp
+        arr = input.split(" ")
+        letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        
+        if arr.length != 2 
+            raise "not right number of numbers, please enter two"
+        elsif 
+            input.split("").length != 3
+                raise "only one space between numbers"
+        elsif
+            arr.any? { |ele| letters.include?(ele)  }
+                raise "use only numbers"
+        else
+            return arr.map! { |ele| ele.to_i }
+        end
+        
     
 
+    end
+
 end
+
+
