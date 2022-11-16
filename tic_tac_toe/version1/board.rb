958f282af41cb3f9443a5ad4f973c88e5ce7aa8a
meihuang <meihuang8328@gmail.com>
2022-11-16 17:05:16 -0500

    part 2: win

--- tic_tac_toe/version1/board.rb
+++ tic_tac_toe/version1/board.rb
@@ -35,7 +35,98 @@ class Board
 
     end
 
+    def print
+
+        @grid.each do |row|
+            p row 
+        end
+
+    end
+
+    def win_row?(mark)
+
+        @grid.each do |row|
+            return true if row.all?(mark)
+        end
+
+        false 
+
+    end
     
+    def win_col?(mark)
+
+        columns = @grid.transpose
+        columns.each do |col|
+            return true if col.all?(mark)
+        end
+
+        false
+
+    end
+
+    def win_diagonal?(mark)
+        count = 0
+        len = @grid.length
+        num = 0
+        idx = 0
+        num1 = @grid.length - 1
+
+            while num < len
+                while idx < len
+                    if @grid[idx][num] == mark
+                        count += 1
+                    end
+                    num += 1
+                    idx += 1
+                end
+            end
+
+        if count == len
+            return true
+        else
+            count = 0
+            idx = 0
+            while num1 >= 0
+                while idx < len
+                    if @grid[idx][num1] == mark
+                        count += 1
+                    end
+                    num1 -= 1
+                    idx += 1
+                end
+            end
+        end
+
+        count == len
+
+    end
+
+    # def win_diagonal?(mark)
+
+    #     r_grid = @grid.reverse
+
+    #     diag = []
+    #     other_diag = []
+
+    #     @grid.each.with_index {|row, i1| row.each.with_index {|val, i2| diag << val if i1 == i2}}
+    #     r_grid.each.with_index {|row, i1| row.each.with_index {|val, i2| other_diag << val if i1 == i2}}
+
+    #     diag.all?(mark) || other_diag.all?(mark)
+
+    # end
+
+    def win?(mark)
+
+       return true if win_row?(mark) || win_col?(mark) || win_diagonal?(mark)
+       false
+
+    end
+
+    def empty_positions?
+
+       @grid.flatten!.any?(:_)
+
+    end
 
 end
 
@@ -49,3 +140,11 @@ p b = Board.new
 #p b.place_mark([1,4], "X") # Invalid spot error
 #p b.place_mark([0,0], :x)
 #p b.place_mark([0,0], "o") # Invalid not empty
+#b.print
+
+b.place_mark([0, 2], :O)
+b.place_mark([1,1], :O)
+ b.place_mark([2,0], :O)
+ b.print
+ 
+ p b.empty_positions?
