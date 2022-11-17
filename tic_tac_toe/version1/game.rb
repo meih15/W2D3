e327e382b12eb575d578ae2ffb8fd8f75c55a348
meihuang <meihuang8328@gmail.com>
2022-11-16 21:58:10 -0500

    game

--- tic_tac_toe/version1/game.rb
+++ tic_tac_toe/version1/game.rb
@@ -4,12 +4,40 @@ require_relative "human_player"
 class Game
 
     def initialize(player_1_mark, player_2_mark)
-        @current_player = player_1
+        @player_1 = HumanPlayer.new(player_1_mark)
+        @player_2 = HumanPlayer.new(player_2_mark)
+        @current_player = @player_1
+        @board = Board.new
     end
 
-    attr_reader :current_player
+    attr_reader :current_player, :player_1, :player_2, :board
+
+    def switch_turn
+        if @current_player == @player_1
+            @current_player = @player_2
+        else
+            @current_player = @player_1
+        end
+    end
+
+    def play 
+        while @board.empty_positions?
+            @board.print
+            position = @current_player.get_position
+            @board.place_mark(position, @current_player.mark)
+            if @board.win?(@current_player.mark)
+                p @current_player.mark.to_s + "has won!"
+                return
+            else
+                self.switch_turn
+            end
+        end
+
+        puts "game over"
+        puts "draw"
+
+    end
 
     
-
 
 end
